c34081f995b30ec02c79f55cdd031d979ad90acd
ro7m <robin.mundra@gmail.com>
2024-12-27 09:22:23 -0800

    transform to ocrservice

--- lib/models/bounding_box.dart
+++ lib/models/bounding_box.dart
@@ -1,5 +1,3 @@
-import 'dart:math' as math;
-
 class BoundingBox {
   final double x;
   final double y;
@@ -13,22 +11,4 @@ class BoundingBox {
     required this.height,
   });
 
-double clamp(double number, double size) {
-  return math.max(0, math.min(number, size));
-}
-
-  BoundingBox transformBoundingBox(BoundingBox contour, int id, List<int> size) {
-  double offset = (contour.width * contour.height * 1.8) / (2 * (contour.width + contour.height));
-  double p1 = clamp(contour.x - offset, size[1].toDouble()) - 1;
-  double p2 = clamp(p1 + contour.width + 2 * offset, size[1].toDouble()) - 1;
-  double p3 = clamp(contour.y - offset, size[0].toDouble()) - 1;
-  double p4 = clamp(p3 + contour.height + 2 * offset, size[0].toDouble()) - 1;
-
-  return BoundingBox(
-    x: p1 / size[1],
-    y: p3 / size[0],
-    width: (p2 - p1) / size[1],
-    height: (p4 - p3) / size[0],
-  );
-}
 }
--- lib/services/ocr_service.dart
+++ lib/services/ocr_service.dart
@@ -7,7 +7,6 @@ import 'image_preprocessor.dart';
 import 'text_detector.dart';
 import 'text_recognizer.dart';
 import 'dart:math' as math;
-import '../constants.dart';
 
 class OCRService {
   final ModelLoader modelLoader = ModelLoader();
@@ -24,6 +23,25 @@ class OCRService {
     await modelLoader.loadModels(debugCallback: debugCallback);
     textDetector = TextDetector(modelLoader.detectionModel!);
     textRecognizer = TextRecognizer(modelLoader.recognitionModel!);
+  }
+
+  double clamp(double number, double size) {
+    return math.max(0, math.min(number, size));
+  }
+
+  BoundingBox transformBoundingBox(BoundingBox contour, int id, List<int> size) {
+    double offset = (contour.width * contour.height * 1.8) / (2 * (contour.width + contour.height));
+    double p1 = clamp(contour.x - offset, size[1].toDouble()) - 1;
+    double p2 = clamp(p1 + contour.width + 2 * offset, size[1].toDouble()) - 1;
+    double p3 = clamp(contour.y - offset, size[0].toDouble()) - 1;
+    double p4 = clamp(p3 + contour.height + 2 * offset, size[0].toDouble()) - 1;
+
+    return BoundingBox(
+      x: p1 / size[1],
+      y: p3 / size[0],
+      width: (p2 - p1) / size[1],
+      height: (p4 - p3) / size[0],
+    );
   }
 
   Future<List<OCRResult>> processImage(ui.Image image, {void Function(String)? debugCallback}) async {
